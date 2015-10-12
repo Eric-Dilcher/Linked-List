ab23fadf589d7042481422e2421061092373613e
Eric Dilcher <eric.d.opensource@gmail.com>
2015-10-12 17:36:07 +0200

    added addFirst, addLast, deleteFirst, deleteLast etc... methods

--- LinkedList.h
+++ LinkedList.h
@@ -11,6 +11,8 @@ class LinkedList{
 
 private:
 
+  int num_elements;
+
   Node<T> *head;
 
   Node<T> * getNodeAtPos(int position);
@@ -23,13 +25,23 @@ public:
 
   ~LinkedList();
 
-  void prepend(T data);
+  int size();
 
-  void append(T data);
+  void addFirst(T data);
+
+  void addLast(T data);
 
   void addAtPos(T data, int position);
 
+  T getFirst();
+
+  T getLast();
+
   T getAtPos(int position);
+
+  T deleteFirst();
+
+  T deleteLast();
 
   T deleteAtPos(int position);
 
@@ -44,11 +56,17 @@ public:
 template <class T>
 LinkedList<T>::LinkedList(){
   head = nullptr;
+  num_elements = 0;
 }
 
 template <class T>
 LinkedList<T>::~LinkedList(){
   clearList();
+}
+
+template <class T>
+int LinkedList<T>::size(){
+  return num_elements;
 }
 
 template <class T>
@@ -70,44 +88,46 @@ Node<T> * LinkedList<T>::getNodeAtPos(int position){
 
 template <class T>
 T LinkedList<T>::delete_node_after(Node<T> * n){
-    Node<T> * temp = n->next;
-    T to_return = temp->data;
-    n->next = temp->next;
-    delete temp;
-    return to_return;
+  Node<T> * temp = n->next;
+  T to_return = temp->data;
+  n->next = temp->next;
+  delete temp;
+  num_elements -=1;
+  return to_return;
 }
 
 template <class T>
-void LinkedList<T>::prepend(T data){
-  Node<T> *new_head = new Node<T>(data, head);
-  head = new_head;
+void LinkedList<T>::addFirst(T data){
+  addAtPos(data, 0);
 }
 
 template <class T>
-void LinkedList<T>::append(T data){
-  Node<T> *new_tail = new Node<T>(data, nullptr);
-  Node<T> *temp = head;
-  if(isEmpty()){
-    head = new_tail;
-  }
-  else{
-    while(temp->next != nullptr){
-      temp = temp->next;
-    }
-    temp->next = new_tail;
-  }
+void LinkedList<T>::addLast(T data){
+  addAtPos(data, size());
 }
 
 template <class T>
 void LinkedList<T>::addAtPos(T data, int position){
   if(position == 0){
-    prepend(data);
+    Node<T> *new_head = new Node<T>(data, head);
+    head = new_head;
   }
   else{
     Node<T> *before = getNodeAtPos(position - 1);
     Node<T> *after = before->next;
     before->next = new Node<T>(data, after);
   }
+  num_elements += 1;
+}
+
+template <class T>
+T LinkedList<T>::getFirst(){
+  return getAtPos(0);
+}
+
+template <class T>
+T LinkedList<T>::getLast(){
+  return getAtPos(size() - 1);
 }
 
 template <class T>
@@ -116,11 +136,22 @@ T LinkedList<T>::getAtPos(int position){
 }
 
 template <class T>
+T LinkedList<T>::deleteFirst(){
+  return deleteAtPos(0);
+}
+
+template <class T>
+T LinkedList<T>::deleteLast(){
+  return deleteAtPos(size() - 1);
+}
+
+template <class T>
 T LinkedList<T>::deleteAtPos(int position){
   if(isEmpty()){
     throw(std::out_of_range("List is empty"));
   }
   else if(position == 0){
+    num_elements -= 1;
     Node<T> *temp = head;
     T to_return = temp->data;
     head = temp->next;
@@ -140,6 +171,7 @@ void LinkedList<T>::clearList(){
   //  do nothing
   }
   else{
+    num_elements = 0;
     Node<T> * temp = head;
     head = nullptr;
     Node<T>  * next = nullptr;
--- LinkedListTestFloat.cpp
+++ LinkedListTestFloat.cpp
@@ -8,10 +8,10 @@ struct LinkedListSetup{
   LinkedList<float> * ll;
   LinkedListSetup(){
     ll = new LinkedList<float>();
-    ll->append(1.1);
-    ll->append(2.2);
-    ll->append(3.3);
-    ll->append(4.4);
+    ll->addLast(1.1);
+    ll->addLast(2.2);
+    ll->addLast(3.3);
+    ll->addLast(4.4);
   }
   ~LinkedListSetup(){
     delete ll;
@@ -29,25 +29,25 @@ TEST_FIXTURE(LinkedListSetup, TestClearList){
   CHECK_EQUAL(true, ll->isEmpty());
 }
 
-TEST(TestAppend){
-  LinkedList<float> ll;
-  ll.append(1.1);
-  CHECK_CLOSE(1.1, ll.getAtPos(0), EPSILON);
-  ll.append(2.2);
-  CHECK_CLOSE(2.2, ll.getAtPos(1), EPSILON);
-}
-
 TEST_FIXTURE(LinkedListSetup, TestGetAtPos){
   CHECK_CLOSE(1.1, ll->getAtPos(0), EPSILON);
   CHECK_CLOSE(2.2, ll->getAtPos(1), EPSILON);
   CHECK_CLOSE(3.3, ll->getAtPos(2), EPSILON);
 }
 
-TEST_FIXTURE(LinkedListSetup, TestPrepend){
-  ll->prepend(0);
+TEST_FIXTURE(LinkedListSetup, TestAddFirst){
+  ll->addFirst(0);
   CHECK_CLOSE(0,ll->getAtPos(0), EPSILON);
-  ll->prepend(-1.1);
+  ll->addFirst(-1.1);
   CHECK_CLOSE(-1.1, ll->getAtPos(0), EPSILON);
+}
+
+TEST(TestAddLast){
+  LinkedList<float> ll;
+  ll.addLast(1.1);
+  CHECK_CLOSE(1.1, ll.getAtPos(0), EPSILON);
+  ll.addLast(2.2);
+  CHECK_CLOSE(2.2, ll.getAtPos(1), EPSILON);
 }
 
 TEST_FIXTURE(LinkedListSetup, TestAddAtPos){
@@ -57,10 +57,45 @@ TEST_FIXTURE(LinkedListSetup, TestAddAtPos){
   CHECK_CLOSE(100, ll->getAtPos(3), EPSILON);
 }
 
+TEST_FIXTURE(LinkedListSetup, TestGetFirst){
+  CHECK_CLOSE(1.1, ll->getFirst(), EPSILON);
+}
+
+TEST_FIXTURE(LinkedListSetup, TestGetLast){
+  CHECK_CLOSE(4.4, ll->getLast(), EPSILON);
+}
+
 TEST_FIXTURE(LinkedListSetup, TestDeleteAtPos){
   CHECK_CLOSE(2.2, ll->deleteAtPos(1), EPSILON);
   CHECK_CLOSE(3.3, ll->deleteAtPos(1), EPSILON);
   CHECK_CLOSE(4.4, ll->deleteAtPos(1), EPSILON);
   CHECK_CLOSE(1.1, ll->deleteAtPos(0), EPSILON);
   CHECK(ll->isEmpty());
+}
+
+TEST_FIXTURE(LinkedListSetup, TestDeleteFirst){
+  CHECK_CLOSE(1.1, ll->deleteFirst(), EPSILON);
+  CHECK_EQUAL(3, ll->size());
+}
+
+TEST_FIXTURE(LinkedListSetup, TestDeleteLast){
+  CHECK_CLOSE(4.4, ll->deleteLast(), EPSILON);
+  CHECK_EQUAL(3, ll->size());
+}
+
+TEST_FIXTURE(LinkedListSetup, TestSize){
+  CHECK_EQUAL(4, ll->size());
+  ll->addFirst(0.1);
+  CHECK_EQUAL(5, ll->size());
+  ll->addAtPos(2.3, 2);
+  CHECK_EQUAL(6, ll->size());
+  ll->addLast(99.99);
+  CHECK_EQUAL(7, ll->size());
+  ll->deleteFirst();
+  CHECK_EQUAL(6, ll->size());
+  ll->deleteLast();
+  CHECK_EQUAL(5, ll->size());
+  ll->deleteAtPos(1);
+  CHECK_EQUAL(4, ll->size());
+
 }
